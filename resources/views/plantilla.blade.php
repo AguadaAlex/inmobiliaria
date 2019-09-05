bf7f1637e2839f69f7f90d6701fa864f6b8259de
aguada <david@aguada.com>
2019-09-04 22:07:12 -0300

    cargando los js y css para arrancar el funcionamiento de bootstrap

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -2,10 +2,6 @@
 <html lang="en">
 <head>
     @include('modulos/head')
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 </head>
 <body>
     <section>
@@ -23,6 +19,7 @@
             @include('modulos/pie')
         </div>
 
-    </section>    
+    </section> 
+    @include('modulos/script')
 </body>
 </html>
